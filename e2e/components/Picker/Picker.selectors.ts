cfa27dbd9f77ca6fcafa57044ccf9c2ca38ca5d9
Jakub Ryłko <jakub.rylko@gmail.com>
2025-01-20 15:18:41 +0100

    Cover edge case for Android

--- e2e/components/Picker/Picker.selectors.ts
+++ e2e/components/Picker/Picker.selectors.ts
@@ -3,12 +3,12 @@ import { getById, getByType, getWithAncestor } from 'e2e/helpers'
 export const dateCancelBtn = getById('datePicker-cancel')
 export const dateConfirmBtn = getById('datePicker-confirm')
 export const datePicker = getById('datePicker')
+export const datePickerText = getWithAncestor(
+  by.type('android.widget.TextView'),
+  by.type('android.widget.DatePicker')
+)
 export const hourPicker = getByType('android.widget.EditText').atIndex(0)
 export const minutesPicker = getByType('android.widget.EditText').atIndex(1)
 export const timeCancelBtn = getById('timePicker-cancel')
 export const timeConfirmBtn = getById('timePicker-confirm')
 export const timePicker = getById('timePicker')
-export const yearPicker = getWithAncestor(
-  by.type('android.widget.TextView'),
-  by.type('android.widget.DatePicker')
-).atIndex(0)
