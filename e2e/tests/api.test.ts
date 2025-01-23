1e1dfaafcd59929bd0762119f318bdd8b92b8772
Jakub Ryłko <jakub.rylko@gmail.com>
2025-01-23 09:55:07 +0100

    Add android workflow

--- e2e/tests/api.test.ts
+++ e2e/tests/api.test.ts
@@ -1,8 +1,7 @@
 import { makeRequest } from 'e2e/helpers'
 
 describe('API', () => {
-  it('Get image', async () => {
-    const response = await makeRequest('GET', '/assets/src/assets/paris.jpeg')
-    console.log(response)
+  it('Get asset', async () => {
+    await makeRequest('GET', '/assets/src/assets/paris.jpeg')
   })
 })
