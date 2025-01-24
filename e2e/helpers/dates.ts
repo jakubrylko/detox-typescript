a5761837934a957e9437672bda60c2e4383938fe
Jakub Ryłko <jakub.rylko@gmail.com>
2025-01-24 23:19:54 +0100

    Update android workflow

--- e2e/helpers/dates.ts
+++ e2e/helpers/dates.ts
@@ -3,12 +3,7 @@ import dayjs from 'dayjs'
 
 export const getCurrentYear = () => new Date().getFullYear()
 
-export const getRandomTime = () => {
-  const randomDate = faker.date.soon({ days: 7 })
-  const hour = String(randomDate.getHours()).padStart(2, '0')
-  const minutes = String(randomDate.getMinutes()).padStart(2, '0')
-  return `${hour}:${minutes}`
-}
+export const getRandomTime = () => dayjs(faker.date.soon()).format('HH:mm')
 
 export const convertDateToDashedFormat = (dateString: string) =>
   dayjs(dateString).format('DD-MM-YYYY')
