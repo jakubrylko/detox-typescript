df494304f688a73de50944ba09cfba06fb20a3b9
Jakub Ryłko <jakub.rylko@gmail.com>
2025-01-24 13:31:09 +0100

    Update iOS workflow

--- jest.config.js
+++ jest.config.js
@@ -4,12 +4,11 @@ module.exports = {
   maxWorkers: 1,
   moduleNameMapper: { 'e2e/(.*)': '<rootDir>/e2e/$1' },
   preset: 'react-native',
+  reporters: ['detox/runners/jest/reporter'],
   setupFilesAfterEnv: ['<rootDir>/jest.setup.js'],
   testEnvironment: 'detox/runners/jest/testEnvironment',
   testMatch: ['<rootDir>/e2e/**/*.test.ts'],
   testTimeout: 200000,
   transform: { '\\.(jpg|jpeg|png|gif|webp|svg)$': 'jest-transform-stub' },
-  verbose: true,
-
-  reporters: ['detox/runners/jest/reporter', { resultsDir: 'e2e/artifacts' }]
+  verbose: true
 }
